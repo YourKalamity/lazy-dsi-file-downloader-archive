057e5550f9f7f8ef477aacab66ae569b19012840
YourKalamity <63474858+YourKalamity@users.noreply.github.com>
2022-05-01 10:55:01 +0100

    Switch to requirements.txt

--- cxfreeze_setup.py
+++ cxfreeze_setup.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 
 from cx_Freeze import setup, Executable
+import platform
 
-# Dependencies are automatically detected, but it might need fine tuning.
-# "packages": ["os"] is used as example only
 build_exe_options = {'build_exe': "dist"}
 
-# base="Win32GUI" should be used only for Windows GUI app
-base = "Win32GUI"
+base = None
+if platform.system() == "Windows":
+    base = "Win32GUI"
 
 setup(
     name="lazy-dsi-file-downloader",
-    version="3.1.3",
+    version="3.1.5",
     description="Lazy DSi File Downloader",
     options={"build_exe": build_exe_options},
     executables=[Executable("main.py", base=base)],
