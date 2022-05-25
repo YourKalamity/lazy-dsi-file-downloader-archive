c97adf76c856e4b3693b9f22d9bbd6f32bf27d27
YourKalamity <63474858+YourKalamity@users.noreply.github.com>
2022-05-25 21:52:31 +0100

    Add console window for Windows builds

--- cxfreeze_setup.py
+++ cxfreeze_setup.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
 
 from cx_Freeze import setup, Executable
-import platform
 
 build_exe_options = {'build_exe': "dist"}
-
 base = None
-if platform.system() == "Windows":
-    base = "Win32GUI"
 
 setup(
     name="lazy-dsi-file-downloader",
-    version="3.1.5",
+    version="3.2.2",
     description="Lazy DSi File Downloader",
     options={"build_exe": build_exe_options},
     executables=[Executable("main.py", base=base)],
